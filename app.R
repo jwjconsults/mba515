13c0610ed4103d0e8ef1035cb4adfb89af7825f1
jwjconsults <jwjohnson1@captechu.edu>
2022-08-02 21:45:23 -0400

    Working Data display
    Interactive display allowing user to select the data they wish to see with a drop down menu

--- app.R
+++ app.R
@@ -1,56 +1,73 @@
-####################################
-# Data Professor                   #
-# http://youtube.com/dataprofessor #
-# http://github.com/dataprofessor  #
-####################################
-
-# Modified from Winston Chang, 
-# https://shiny.rstudio.com/gallery/shiny-theme-selector.html
-
-# Concepts about Reactive programming used by Shiny, 
-# https://shiny.rstudio.com/articles/reactivity-overview.html
-
-# Load R packages
 library(shiny)
 library(shinythemes)
+library(xlsx)
+library(dplyr)
+library(ggplot2)
+library(scales)
 
+# ui <- fluidPage(
+#   h1("Bar Plot using GGPlot"),
+#   plotOutput("plot")
+# )
+# 
+# server <- function(input, output, session) {
+#   
+#   output$plot <- renderPlot({
+#     df1 <- read.xlsx("Offenses_Completed_and_Attempted.xlsx", sheetIndex = 1)
+#     df = df1[3:28 ,]
+#     #Plot
+#     ggplot(df) + geom_col(aes(x = Total.Offenses, y = Offense.Category), stat = "identity")
+#   })
+# }
 
-  # Define UI
-  ui <- fluidPage(theme = shinytheme("cerulean"),
-    navbarPage(
-       #theme = "superhero",  # <--- To use a theme, uncomment this
-      "MBA 515 Crime Data",
-      tabPanel("Data By State",
-               sidebarPanel(
-                 tags$h3("Input:"),
-                 textInput("txt1", "Given Name:", ""),
-                 textInput("txt2", "Surname:", ""),
-                 
-               ), # sidebarPanel
-               mainPanel(
-                            h1("Header 1"),
-                            
-                            h4("Output 1"),
-                            verbatimTextOutput("txtout"),
+ui <- fluidPage(theme = shinytheme("cerulean"),
+                navbarPage("MBA 515",
+                           
+                           tabPanel("Offenses Attempted and Completed",
+                                    
+                                    sidebarPanel(
+                                      selectInput(inputId = "sel_Offenses",
+                                                  label = "Choose Attempted or Completed",
+                                                  "Names")
+                                    ), #Sidebar Panel
+                                    
+                                    mainPanel(
+                                      plotOutput("plot")
+                                    ) #Main End
+                           ), #  Tab 1 End
+                           tabPanel("Navbar 2", "This panel is intentionally left blank"),
+                           tabPanel("Navbar 3", "This panel is intentionally left blank")
+                           
+                ) #NavBar Page End
+) #UI End
 
-               ) # mainPanel
-               
-      ), # Navbar 1, tabPanel
-      tabPanel("Navbar 2", "This panel is intentionally left blank"),
-      tabPanel("Navbar 3", "This panel is intentionally left blank")
+server <- function(input, output, session) {
   
-    ) # navbarPage
-  ) # fluidPage
-
   
-  # Define server function  
-  server <- function(input, output) {
+  df1 <- read.xlsx("Offenses_Completed_and_Attempted.xlsx", sheetIndex = 1)
+  # Summarize Data and Plot Chart - 
+  #wrap in reactive to allow selection of data
+  dataSelected <- reactive({
+    req(input$sel_Offenses)
     
-    output$txtout <- renderText({
-      paste( input$txt1, input$txt2, sep = " " )
-    })
-  } # server
+    
+  })
   
+  # Update SelectInput Dynamically
+  observe({
+    updateSelectInput(session, "sel_Offenses", choices = colnames(df1[2:4]))
+  })
+  #Plot ggplot2
+  # Wrap in render cmd to make interactive
+  
+  output$plot <- renderPlot({
+    df = df1[3:28 ,]
+    ggplot(df)+ geom_col(aes_string(x = input$sel_Offenses, y = "Offense.Category"))+ scale_x_continuous(labels = comma)
+    
+  })
+  
+  
+}
 
-  # Create Shiny object
-  shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)
+
